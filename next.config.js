86303c661b64cdbb13f3a9be2f9ee833adda7a18
Danny Anderson <daniel.anderson@ccr.net>
2025-04-05 18:42:35 -0500

    fix: disable PostCSS processing during build

--- next.config.js
+++ next.config.js
@@ -7,12 +7,26 @@ const nextConfig = {
         hostname: 'm.media-amazon.com',
       },
     ],
+    domains: ['romance.io'],
   },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    // Disable PostCSS processing
+    config.module.rules.forEach(rule => {
+      if (rule.oneOf) {
+        rule.oneOf.forEach(oneOfRule => {
+          if (oneOfRule.use && oneOfRule.use.some(use => use.loader && use.loader.includes('postcss-loader'))) {
+            oneOfRule.use = oneOfRule.use.filter(use => !use.loader || !use.loader.includes('postcss-loader'));
+          }
+        });
+      }
+    });
+    return config;
   }
 };
 
