bfce05d1422a2d5bdad616b69a43ae004797cc7e
Danny Anderson <daniel.anderson@ccr.net>
2025-04-05 18:45:52 -0500

    fix: add PostCSS dependencies with matching versions

--- next.config.js
+++ next.config.js
@@ -14,23 +14,6 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    // Disable PostCSS by removing the loader
-    config.module.rules = config.module.rules.map(rule => {
-      if (rule.oneOf) {
-        rule.oneOf = rule.oneOf.map(oneOfRule => {
-          if (oneOfRule.use && Array.isArray(oneOfRule.use)) {
-            oneOfRule.use = oneOfRule.use.filter(loader => 
-              !loader.loader || !loader.loader.includes('postcss-loader')
-            );
-          }
-          return oneOfRule;
-        });
-      }
-      return rule;
-    });
-    return config;
   }
 };
 
