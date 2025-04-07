eb54842ccb1fdec23b0956bd16a689f016676139
Danny Anderson <daniel.anderson@ccr.net>
2025-04-07 09:02:22 -0500

    fix: add back minimal PostCSS dependencies with compatible versions

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
+    }
+  }
+} 
